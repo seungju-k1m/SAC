16f3197a66d01fdec5f471cde8e13f18ed94c8bb
seungju-mmc <ksj970213@hanyang.ac.kr>
2020-11-11 14:02:48 +0900

    changed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from SAC.Trainer import sacOnPolicyTrainer
 
 if __name__ == '__main__':
-    trainer = sacOnPolicyTrainer('./cfg/MacOs.json')
+    trainer = sacOnPolicyTrainer('./cfg/LSTM.json')
     trainer.run()
