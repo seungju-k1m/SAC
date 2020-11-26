345612a5e4e4651ca1132d028d193794e1c140d1
seungju-mmc <ksj970213@hanyang.ac.kjr>
2020-11-27 03:01:43 +0900

    .

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from SAC.Trainer import sacTrainer
 
 if __name__ == '__main__':
-    trainer = sacTrainer('./cfg/Linux.json')
+    trainer = sacTrainer('./cfg/MacOs.json')
     trainer.run()
     print("Hello")
