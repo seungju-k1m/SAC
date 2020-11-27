d62a6718fa0928439c30aef0862ea4972be4110e
seungju-mmc <ksj970213@hanyang.ac.kr>
2020-11-28 00:33:48 +0900

    .

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from SAC.Trainer import sacTrainer
 
 if __name__ == '__main__':
-    trainer = sacTrainer('./cfg/MacOs.json')
+    trainer = sacTrainer('./cfg/Linux.json')
     trainer.run()
     print("Hello")
