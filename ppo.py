7537452a34fecc5f6a859aaa7264461f78a26d97
seungju-mmc <ksj970213@hanyang.ac.kr>
2020-12-15 13:35:37 +0900

    .

--- ppo.py
+++ ppo.py
@@ -1,6 +1,6 @@
 from PPO.Trainer import PPOOnPolicyTrainer
 
 if __name__ == '__main__':
-    trainer = PPOOnPolicyTrainer('./cfg/LinuxPPOLSTM.json')
+    trainer = PPOOnPolicyTrainer('./cfg/v1.json')
     trainer.run()
     print("Hello")
